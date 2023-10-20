c6f891352a87c3839cd74d1084395f5c49e62d2b
Madeliene <madelienestiller@gmail.com>
2023-10-20 10:08:40 +1000

    phase 1 array lab complete

--- index.js
+++ index.js
@@ -1,49 +1,41 @@
-const cats = ['Milo', 'Otis', 'Garfield'];
-
+const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
-    cats.push(name);
-    return cats;
+  cats.push(name);
+  return cats;
 }
-
 
 function destructivelyPrependCat(name) {
-    cats.unshift(name);
-    return cats;
+  cats.unshift(name);
+  return cats;
 }
 
-
-function destructivelyRemoveLastCat(name) {
-    cats.pop(name);
-    return cats;
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
 }
 
-
-function destructivelyRemoveFirstCat(name) {
-    cats.shift(name);
-    return cats;
-    
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
 }
 
 function appendCat(name) {
-    const endCat = [...cats, name];
-    return endCat
+  const endCat = [...cats, name];
+  return endCat;
 }
 
 function prependCat(name) {
-    const startCat = [name, ...cats]
-    return startCat
+  const startCat = [name, ...cats];
+  return startCat;
 }
 
-// create a function that removes the last cat in the cats array and returns a new array, leaving the cats array unchanged.
-//Hint: We should make some copies.
 function removeLastCat() {
-    const byeCat = cats.slice(0, cats.length - 1);
-    return byeCat
+  const byeCat = cats.slice(0, cats.length - 1);
+  return byeCat;
 }
 
-// create a function that removes the last cat in the cats array and returns a new array, leaving the cats array unchanged.
 function removeFirstCat() {
-    const helloCat = cats.slice(1)
-    return helloCat
+  const byeFirstCat = cats.slice(1);
+  return byeFirstCat;
 }
